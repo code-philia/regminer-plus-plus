9eebdb4d6fe3a618f912f17928edbb79dba7707e
zhj-lu <1368099869@qq.com>
2023-11-23 14:24:04 +0800

    compile all commits of a project

---
+++ miner/src/main/java/regminer/start/CompileProject.java
@@ -0,0 +1,61 @@
+package regminer.start;
+
+import org.builder.EnvConfigLoader;
+import org.builder.api.ProjectBuilder;
+import org.builder.model.CompileResult;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import regminer.constant.Conf;
+import regminer.git.provider.Provider;
+import regminer.miner.migrate.BFCEvaluator;
+import regminer.utils.FileUtilx;
+
+import java.io.File;
+
+public class CompileProject {//This java file is used to compile all commits of a given project
+
+    public static void main(String[] args) throws Exception {
+        EnvConfigLoader.setConfigPath("/Users/zhjlu/codes/graduate/RegMiner/miner/miner.properties");// set to your own properties path!
+        EnvConfigLoader.refresh();
+        ProjectBuilder projectBuilder = new ProjectBuilder();
+
+        ConfigLoader.setConfigPath("/Users/zhjlu/codes/graduate/RegMiner/miner/env.properties");
+        ConfigLoader.refresh();
+        Repository repo = new Provider().create(Provider.EXISITING).get(Conf.LOCAL_PROJECT_GIT);
+        Git git = new Git(repo);
+        Iterable<RevCommit> commits = git.log().all().call();
+
+        BFCEvaluator evaluator = new BFCEvaluator(repo);
+        int commitCnt = 0;
+        int successCnt = 0;
+        for (RevCommit commit : commits) {
+            commitCnt++;
+            if (commitCnt < 64) {
+                continue;
+            }
+            if (commitCnt > 70) {
+                break;
+            }
+            String commitId = commit.getName();
+            File checkedOutDir = evaluator.checkout(commitId, commitId, "commit");
+            projectBuilder.setProjectDir(checkedOutDir);
+            CompileResult result = projectBuilder.compile();
+            evaluator.emptyCache(commitId);
+
+            if (result.getState() == CompileResult.CompileState.SUCCESS) {
+                successCnt++;
+            } else {
+                System.out.println(result.getState() + ": " + commitId);
+                FileUtilx.log(result.getState() + ": " + commitId);
+            }
+            System.out.println("Now: " + successCnt + " of " + commitCnt);
+            FileUtilx.log("Now: " + successCnt + " of " + commitCnt);
+        }
+
+        System.out.println(commitCnt + " commits!");
+        System.out.println(successCnt + " successes!");
+        FileUtilx.log(commitCnt + " commits!");
+        FileUtilx.log(successCnt + " successes!");
+    }
+}
